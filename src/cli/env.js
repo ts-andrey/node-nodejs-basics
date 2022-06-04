3da8108b65fd4f6b3478acf76223766796055cc1
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-06-04 22:29:31 +0300

    feat: add cli solution 2022-06-04 (22:29:31)

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,18 @@
+import { env } from 'process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  let result = '';
+  const vars = env.npm_lifecycle_script.split(' ').slice(2);
+  vars.forEach(el => {
+    if (el.startsWith('RSS_')) {
+      if (result === '') {
+        result += el;
+      } else {
+        result += `; ${el}`;
+      }
+    }
+  });
+  console.log(result);
 };
+
+parseEnv();
