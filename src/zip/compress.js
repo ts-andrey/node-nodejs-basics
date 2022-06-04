b644dccd0c65f6f7266f75ac0284b07339f0a29a
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-06-05 01:30:06 +0300

    feat: add zip solution 2022-06-05 (01:30:06)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const WORK_PATH = path.join(process.cwd(), './src/zip/');
+const FILE_PATH = './files/fileToCompress.txt';
+const FINAL_PATH = './files/archive.gz';
+
 export const compress = async () => {
-    // Write your code here 
+  //file-name : compress-file-using-zlib.js
+  // Including the required modules
+  var zip = zlib.createGzip();
+
+  var read = fs.createReadStream(path.join(WORK_PATH, FILE_PATH));
+  var write = fs.createWriteStream(path.join(WORK_PATH, FINAL_PATH));
+  //Transform stream which is zipping the input file
+  read.pipe(zip).pipe(write);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import zlib from 'zlib';
+
+const WORK_PATH = path.join(process.cwd(), './src/zip/');
+const FILE_PATH = './files/archive.gz';
+const FINAL_PATH = './files/fileToCompress.txt';
+
 export const decompress = async () => {
-    // Write your code here 
+  //file-name : compress-file-using-zlib.js
+  // Including the required modules
+  var unzip = zlib.createUnzip();
+
+  var read = fs.createReadStream(path.join(WORK_PATH, FILE_PATH));
+  var write = fs.createWriteStream(path.join(WORK_PATH, FINAL_PATH));
+  //Transform stream which is zipping the input file
+  read.pipe(unzip).pipe(write);
 };
+
+decompress();
