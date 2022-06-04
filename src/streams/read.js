a3b52a089fffa168a48c7b4f558c360a3adfdf9a
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2022-06-05 01:15:43 +0300

    feat: add stream solution 2022-06-05 (01:15:43)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,12 @@
+import path from 'path';
+import { createReadStream } from 'fs';
+
+const WORK_PATH = path.join(process.cwd(), './src/streams/');
+const FILE_PATH = './files/fileToRead.txt';
+
 export const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(path.join(WORK_PATH, FILE_PATH));
+  readStream.on('data', chunk => process.stdout.write(chunk, 'utf8'));
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+
+const WORK_PATH = path.join(process.cwd(), './src/streams/');
+const FILE_PATH = './files/fileToWrite.txt';
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = createWriteStream(path.join(WORK_PATH, FILE_PATH));
+
+  process.stdin.on('data', data => {
+    writeStream.write(data, 'utf8');
+  });
 };
+
+write();
